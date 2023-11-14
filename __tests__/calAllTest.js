40d75f340d5d9cf1ebe4aa3c74fb53035f90db64
Hchan2 <seongchan_@naver.com>
2023-11-14 22:27:58 +0900

    feat: 전체 이벤트 할인 계산 테스트

---
+++ __tests__/calAllTest.js
@@ -0,0 +1,28 @@
+import { calSpecialEvent, calWeekdayEvent, calWeekendEvent } from "../src/utils/calDateEvent";
+import calTotalAmount from "../src/utils/calTotalAmount";
+
+describe('메뉴 구매 계산 테스트', () => {
+    test('총 계산 금액 확인하기', () => {
+        const totalAmountTest = calTotalAmount("시저샐러드-1,티본스테이크-1");
+
+        expect(totalAmountTest).toEqual(63000);
+    });
+
+    test('주말 할인 금액 구하기', () => {
+        const totalAmountTest = calWeekendEvent("15", "시저샐러드-1,티본스테이크-1");
+
+        expect(totalAmountTest).toEqual(2023);
+    });
+
+    test('평일 할인 금액 확인하기', () => {
+        const totalAmountTest = calWeekdayEvent("5", "시저샐러드-1,아이스크림-1");
+
+        expect(totalAmountTest).toEqual(2023);
+    });
+
+    test('스페셜 할인 금액 확인하기', () => {
+        const totalAmountTest = calSpecialEvent("3");
+
+        expect(totalAmountTest).toEqual(1000);
+    });
+});
