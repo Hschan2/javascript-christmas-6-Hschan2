0de1ea671b64f52fe8fc2593cad8ea4e2079da2c
Hchan2 <seongchan_@naver.com>
2023-11-15 20:48:53 +0900

    fix: 수정된 내용에 맞게 테스트 코드 수정

--- __tests__/classTest.js
+++ __tests__/classTest.js
@@ -4,21 +4,16 @@ import storeValues from "../src/for-test/storeValues";
 
 describe('출력을 위한 값 테스트', () => {
     test('Date 클래스 저장 값 확인하기', () => {
-        const newDate = new storeDate();
-        newDate.setData("3");
+        const newDate = new storeDate("3");
+        const numberOfDate = Number(newDate.getDate());
 
-        expect(newDate.getDate()).toEqual(3);
+        expect(numberOfDate).toEqual(3);
     });
 
     test('Order 클래스 저장 값 확인하기', () => {
-        const newOrder = new storeOrder();
-        newOrder.setDate("3");
-        newOrder.setOrder("티본스테이크-1,시저샐러드-1");
-        newOrder.setTotalAmount(63000);
+        const newOrder = new storeOrder("티본스테이크-1,시저샐러드-1");
 
-        expect(newOrder.getDate()).toEqual("3");
         expect(newOrder.getOrder()).toEqual("티본스테이크-1,시저샐러드-1");
-        expect(newOrder.getTotalAmount()).toEqual(63000);
     });
 
     test('클래스 내 모든 데이터 저장 확인하기', () => {
