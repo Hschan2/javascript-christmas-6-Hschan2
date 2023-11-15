d723fe73093ff7ec47935039c6c16404aef20c79
Hchan2 <seongchan_@naver.com>
2023-11-15 20:33:09 +0900

    fix: storeDate, storeOrder 클래스를 사용해 유효성 검사 진행되도록 수정

--- src/valid/inputValid.js
+++ src/valid/inputValid.js
@@ -1,20 +1,21 @@
 import InputView from "../InputView";
-import checkAllDateError from "../error/dateError";
 import checkAllOrderError from "../error/orderError";
+import storeDate from "../model/storeDate";
+import storeOrder from "../model/storeOrder";
 
 const inputValid = {
     async validDate() {
         const getDate = await InputView.readDate();
-        checkAllDateError(getDate);
+        const validDate = new storeDate(getDate);
 
-        return Number(getDate);
+        return Number(validDate.getDate());
     },
 
     async validOrder() {
         const getOrder = await InputView.readOrder();
-        checkAllOrderError(getOrder);
+        const validOrder = new storeOrder(getOrder);
 
-        return getOrder;
+        return validOrder.getOrder();
     },
 }
 
