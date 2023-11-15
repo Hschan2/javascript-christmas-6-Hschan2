487eb68a6d439de92991cee74a2e0e7cf4c90f3c
Hchan2 <seongchan_@naver.com>
2023-11-15 20:34:32 +0900

    fix: 배지 반환 방식 변경

--- src/utils/calBadge.js
+++ src/utils/calBadge.js
@@ -1,13 +1,13 @@
 function calBadge(benefitAmount, presentationAmount) {
-    const allBenefitAmounts = benefitAmount + presentationAmount;
-    let badge = '';
+    const allBenefitAmounts = benefitAmount + presentationAmount[1];
 
-    if (allBenefitAmounts >= 20000) badge = '산타';
-    if (allBenefitAmounts >= 10000) badge = '트리';
-    if (allBenefitAmounts >= 5000) badge = '별';
-    if (allBenefitAmounts < 5000) badge = '없음';
-
-    return badge;
+    return allBenefitAmounts >= 20000
+        ? '산타'
+        : allBenefitAmounts >= 10000
+        ? '트리'
+        : allBenefitAmounts >= 5000
+        ? '별'
+        : '없음';
 }
 
 export default calBadge;
