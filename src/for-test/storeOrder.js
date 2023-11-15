870ddb5411fd4f16290943b91ec6d5c3af0c306c
Hchan2 <seongchan_@naver.com>
2023-11-15 20:48:28 +0900

    fix: model의 storeOrder 클래스 내용처럼 수정

--- src/for-test/storeOrder.js
+++ src/for-test/storeOrder.js
@@ -1,30 +1,48 @@
-class storeOrder {
-    #order
-    #date
-    #totalAmount
+import { isNotIncludedMenus, isNotOneMore, isNotOrderForm, isOrderBeverage, isOverOrder, isOverlapMenu } from "../error/orderError";
 
-    setOrder(order) {
-        this.#order = order;
+class storeOrder {
+    #orderData
+
+    constructor(order) {
+        this.#validate(order);
+        this.#orderData = order;
+    }
+
+    #validate(order) {
+        this.#invalidOneMore(order)
+        this.#invalidIncludedMenus(order)
+        this.#invalidOrderForm(order)
+        this.#validOverlapMenu(order)
+        this.#validOrderBeverage(order)
+        this.#validOverOrder(order)
+    }
+
+    #invalidOneMore(order) {
+        isNotOneMore(order);
+    }
+
+    #invalidIncludedMenus(order) {
+        isNotIncludedMenus(order);
+    }
+
+    #invalidOrderForm(order) {
+        isNotOrderForm(order);
+    }
+
+    #validOverlapMenu(order) {
+        isOverlapMenu(order);
+    }
+
+    #validOrderBeverage(order) {
+        isOrderBeverage(order);
+    }
+
+    #validOverOrder(order) {
+        isOverOrder(order);
     }
 
     getOrder() {
-        return this.#order;
-    }
-
-    setDate(date) {
-        this.#date = date;
-    }
-
-    getDate() {
-        return this.#date;
-    }
-
-    setTotalAmount(totalAmount) {
-        this.#totalAmount = totalAmount;
-    }
-
-    getTotalAmount() {
-        return this.#totalAmount;
+        return this.#orderData;
     }
 }
 
