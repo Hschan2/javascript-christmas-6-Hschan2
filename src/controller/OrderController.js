a5c4d177499c894ec04a41c2064cfc27d5299dd5
Hchan2 <seongchan_@naver.com>
2023-11-21 20:32:40 +0900

    fix: order 값 분리 후 유효성 검사로 수정

--- src/controller/OrderController.js
+++ src/controller/OrderController.js
@@ -1,11 +1,13 @@
 import InputView from "../InputView";
 import storeOrder from "../model/storeOrder";
+import separateMenu from "../utils/separateMenu";
 
 class OrderController {
     static async readOrder() {
         try {
             const getOrder = await InputView.readOrder();
-            const validatedOrder = new storeOrder(getOrder);
+            const separateOrder = separateMenu(getOrder);
+            const validatedOrder = new storeOrder(separateOrder);
             return validatedOrder.getOrder();
         } catch (error) {
             throw error;
