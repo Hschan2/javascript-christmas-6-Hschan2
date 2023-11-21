1413e44f72bf99db6a574fcd4d1cccf20c620a14
Hchan2 <seongchan_@naver.com>
2023-11-21 20:33:33 +0900

    fix: 중복 사용되는 order.split을 공통 사용으로 처리함으로써 제거

--- src/error/orderError.js
+++ src/error/orderError.js
@@ -2,59 +2,40 @@ import { ERROR_MESSAGE } from "../constants/messages";
 import { MENUS, OBJECT } from "../constants/objects";
 
 export function isNotOneMore(order) {
-    const separateOrder = order.split(",");
-
-    for (const eachMenu of separateOrder) {
-        const [_, count] = eachMenu.split("-");
-
+    Object.entries(order).map(([menu, count]) => {
         if (!(count >= OBJECT.minOrderCount)) throw new Error(`${ERROR_MESSAGE.prefix} ${ERROR_MESSAGE.InvalidOrder}`);
-    }
+    });
 }
 
 export function isNotIncludedMenus(order) {
-    const separateOrder = order.split(",");
     const allMenus = Object.keys(MENUS)
         .flatMap(category => Object.keys(MENUS[category]));
 
-    for (const eachMenu of separateOrder) {
-        const [menu, _] = eachMenu.split("-");
-
+    Object.entries(order).map(([menu, count]) => {
         if (!allMenus.includes(menu)) throw new Error(`${ERROR_MESSAGE.prefix} ${ERROR_MESSAGE.InvalidIncludedMenus}`);
-    }
+    });
 }
 
 export function isNotOrderForm(order) {
-    const separateOrder = order.split(",");
     const menuFormatRegex = /^[a-zA-Z가-힣\s]+\-\d+$/;
-
-    for (const eachMenu of separateOrder) {
-        if (!menuFormatRegex.test(eachMenu.trim())) throw new Error(`${ERROR_MESSAGE.prefix} ${ERROR_MESSAGE.InvalidMenuForm}`);
-    }
+    Object.entries(order).map(([menu, count]) => {
+        if (!menuFormatRegex.test(`${menu}-${count}`)) throw new Error(`${ERROR_MESSAGE.prefix} ${ERROR_MESSAGE.InvalidMenuForm}`);
+    });
 }
 
 export function isOverlapMenu(order) {
-    const separateOrder = order.split(",");
     const menuMap = new Set();
 
-    for (const eachMenu of separateOrder) {
-        const [menu, _] = eachMenu.split("-");
-
+    Object.entries(order).map(([menu, count]) => {
         if (menuMap.has(menu)) throw new Error(`${ERROR_MESSAGE.prefix} ${ERROR_MESSAGE.InvalidNotOverlap}`);
         else menuMap.add(menu);
-    }
+    });
 }
 
 export function isOrderBeverage(order) {
-    const separateOrder = order.split(",");
-    let isBeverageOrder = true;
-
-    for (const eachMenu of separateOrder) {
-        const [menu, _] = eachMenu.split("-");
-        if (!MENUS.beverage.hasOwnProperty(menu)) {
-            isBeverageOrder = false;
-            break;
-        }
-    }
+    const isBeverageOrder = Object.entries(order).every(([menu, count]) => {
+        return MENUS.beverage.hasOwnProperty(menu);
+    });
 
     if (isBeverageOrder) {
         throw new Error(`${ERROR_MESSAGE.prefix} ${ERROR_MESSAGE.InvalidNotOnlyOrderedBeverage}`);
@@ -62,13 +43,11 @@ export function isOrderBeverage(order) {
 }
 
 export function isOverOrder(order) {
-    const separateOrder = order.split(",");
     let orderedCount = 0;
 
-    for (const eachMenu of separateOrder) {
-        const [_, count] = eachMenu.split("-");
-        orderedCount += Number(count);
-    }
+    Object.entries(order).map(([menu, count]) => {
+        orderedCount += count;
+    });
 
     if (orderedCount > OBJECT.maxOrderCount) {
         throw new Error(`${ERROR_MESSAGE.prefix} ${ERROR_MESSAGE.InvalidUnderTwentyOrder}`);
