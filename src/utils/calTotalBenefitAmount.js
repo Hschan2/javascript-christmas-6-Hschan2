0dc9a3811a8f6bd7075461d03e3dace4f7812851
Hchan2 <seongchan_@naver.com>
2023-11-28 20:49:35 +0900

    Remove: 불필요한 파일들 제거

--- src/utils/calTotalBenefitAmount.js
+++
@@ -1,7 +0,0 @@
-import { EVENT_BENEFITS_RESULT } from "../constants/objects";
-
-function calTotalBenefitAmount() {
-    return Object.values(EVENT_BENEFITS_RESULT).reduce((acc, value) => acc + value, 0);
-}
-
-export default calTotalBenefitAmount;
