a93e3ed5d2c4fad9f4e54625f74395045481755c
Hchan2 <seongchan_@naver.com>
2023-11-29 18:24:00 +0900

    Fix: 원, 개수 객체를 Objects 파일로 이동

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -37,8 +37,3 @@ export const OUTPUT_MESSAGE = Object.freeze({
 export const OUTPUT_PREVIEW = (date) => {
     return `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`
 }
-
-export const UNITS = Object.freeze({
-    UnitOfAmount: "원",
-    UnitOfSize: "개"
-})
