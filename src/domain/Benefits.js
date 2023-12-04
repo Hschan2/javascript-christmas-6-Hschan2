1c02fdb190e4a0ce7d29699b139753f55235ed5a
Hchan2 <seongchan_@naver.com>
2023-12-04 14:16:47 +0900

    Fix: 혜택 계산 메소드 클래스로 분리 등 리팩토링

--- src/domain/Benefits.js
+++ src/domain/Benefits.js
@@ -1,6 +1,8 @@
 import { OUTPUT_MESSAGE } from "../constants/messages";
-import { ABOUT_EVENT_DATE, MENUS, OBJECT, WEEK_EVENT_MENUS } from "../constants/objects";
+import { OBJECT } from "../constants/objects";
+import calculatePresentation from "../utils/calculatePresentation";
 import Amount from "./Amount";
+import Discount from "./Discount";
 
 class Benefits {
     #benefitsList
@@ -11,78 +13,30 @@ class Benefits {
 
     constructor(date, order) {
         this.#benefitsList = {};
-        this.#getBenefits(date, order);
         this.#allAmount = new Amount(order);
-        this.#presentationResult = this.#presentationEvent(this.#allAmount);
-        this.#badgeResult = this.#badge(this.#benefitsList);
-        this.#benefitsAmount = this.#calBenefitsAmount(this.#benefitsList);
+        this.#calculateBenefits(date, order);
     }
 
-    #getBenefits(date, order) {
-        this.#christmasEvent(date);
-        this.#weekdayEvent(date, order);
-        this.#weekendEvent(date, order);
-        this.#specialEvent(date);
+    #calculateBenefits(date, order) {
+        this.#benefitsList[OUTPUT_MESSAGE.printChristmasDiscount] = Discount.calculateChristmasDiscount(date);
+        this.#benefitsList[OUTPUT_MESSAGE.printWeekdayDiscount] = Discount.calculateWeekdayDiscount(date, order);
+        this.#benefitsList[OUTPUT_MESSAGE.printWeekendDiscount] = Discount.calculateWeekendDiscount(date, order);
+        this.#benefitsList[OUTPUT_MESSAGE.printSpecialDiscount] = Discount.calculateSpecialDiscount(date);
+        this.#benefitsList[OUTPUT_MESSAGE.printPresentationDiscount] = Discount.calculatePresentationDiscount(this.#allAmount.getAllOrderAmount());
+
+        this.#presentationResult = calculatePresentation(this.#allAmount.getAllOrderAmount());
+        this.#benefitsAmount = Amount.calculateBenefitsAmount(this.#benefitsList);
+        this.#badgeResult = this.#badge(this.#benefitsAmount);
     }
 
-    #christmasEvent(date) {
-        if (date >= OBJECT.minDate && date <= OBJECT.christmasDate) {
-            this.#benefitsList[OUTPUT_MESSAGE.printChristmasDiscount] = OBJECT.minDiscountAmount + (OBJECT.increasedDiscountAmount * date) - 100;
-        }
-    }
-
-    #weekdayEvent(date, order) {
-        this.#handleDayEvent(date, order, WEEK_EVENT_MENUS.Dessert, OUTPUT_MESSAGE.printWeekdayDiscount);
-    }
-
-    #weekendEvent(date, order) {
-        this.#handleDayEvent(date, order, WEEK_EVENT_MENUS.Main, OUTPUT_MESSAGE.printWeekendDiscount);
-    }
-
-    #handleDayEvent(date, order, menuType, discountMessage) {
-        let menuCount = 0;
-
-        if ((menuType === WEEK_EVENT_MENUS.Dessert && !ABOUT_EVENT_DATE.WeekendEvent.includes(date)) ||
-            (menuType === WEEK_EVENT_MENUS.Main && ABOUT_EVENT_DATE.WeekendEvent.includes(date))) {
-            Object.entries(order).forEach(([menu, count]) => {
-                if (MENUS[menuType].hasOwnProperty(menu)) menuCount += 1 * count;
-            });
-
-            if (menuCount !== 0) {
-                this.#benefitsList[discountMessage] = menuCount * OBJECT.weekDiscount;
-            }
-        }
-    }
-
-    #specialEvent(date) {
-        if (ABOUT_EVENT_DATE.SpecialEvent.includes(date)) {
-            this.#benefitsList[OUTPUT_MESSAGE.printSpecialDiscount] = OBJECT.minDiscountAmount;
-        }
-    }
-
-    #presentationEvent(allAmount) {
-        if (allAmount.getAllOrderAmount() >= OBJECT.overTwelveThousand) {
-            this.#benefitsList[OUTPUT_MESSAGE.printPresentationDiscount] = OBJECT.Champagne;
-            return '샴페인 1개'
-        }
-        this.#benefitsList[OUTPUT_MESSAGE.printPresentationDiscount] = OBJECT.zero;
-        return '없음';
-    }
-
-    #badge(allBenefitAmount) {
-        const getBenefitsAmount = Object.values(allBenefitAmount).reduce((acc, value) => acc + value, 0);
-
-        return getBenefitsAmount >= OBJECT.santaBadge
+    #badge(benefitsAmount) {
+        return benefitsAmount >= OBJECT.santaBadge
             ? '산타'
-            : getBenefitsAmount >= OBJECT.treeBadge
+            : benefitsAmount >= OBJECT.treeBadge
                 ? '트리'
-                : getBenefitsAmount >= OBJECT.starBadge
+                : benefitsAmount >= OBJECT.starBadge
                     ? '별'
                     : '없음';
-    }
-
-    #calBenefitsAmount(benefitsList) {
-        return Object.values(benefitsList).reduce((acc, value) => acc + value, 0);
     }
 
     getBenefitsList() {
